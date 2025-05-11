03e3d32c0dba9cbb538380b8f44602dd3d6b3c72
Firevall2 <geertsema.simen@gmail.com>
2025-05-11 15:07:32 +0200

    Changed title

--- js/areas.js
+++ js/areas.js
@@ -1,8 +1,8 @@
 // Game Info
 setGameInfo({
-    name: "My Grindcraft Game",                         // The name of your game!
+    name: "Grindtale",                         // The name of your game!
     version: "0.1",                                     // The current version of your game!
-    icon: "images/system/blank.png",                    // Link or path to an icon image for your game!
+    icon: "C:\Users\HP\OneDrive - Stichting Carmelcollege\Afbeeldingen\sans_custom-b7daedf6ac04cd9a0dddd304176b2dee826b2965-s1100-c50.png",                    // Link or path to an icon image for your game!
     ID: "myGrindcraftGame",                             // Your game's ID! Should be unique for every game!
 });
 
